3838befdf2997fd3ebd08f765ace0c8b24c13d11
Haydar Kadıoğlu <a.haydar.kadioglu@hotmail.com>
2022-08-23 21:10:25 +0300

    Delete try.py

--- try.py
+++
@@ -1,9 +0,0 @@
-import sqlite
-
-print("Kaydetmek istediğniz kitabı girin...")
-nam = input("ismi: ")
-writer = input("yazarı: ")
-no = int(input("numarası: "))
-
-sqlite.addbook(nam, writer, no)
-print("başarıyla eklendi")
